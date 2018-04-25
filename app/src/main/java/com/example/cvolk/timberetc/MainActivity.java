6a8f554de0ffd2147077d47227d0fcee61649445
Chris Volkernick <cvolkern@gmail.com>
2018-04-25 18:58:51 -0400

    Initial commit

---
+++ app/src/main/java/com/example/cvolk/timberetc/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.cvolk.timberetc;
+
+import android.Manifest;
+import android.app.Activity;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements PermissionManager.IPermissionManager, ZXingScannerView.ResultHandler {
+
+    private static final int PERMISSIONS_REQUEST_CAMERA = 10;
+    private static final String TAG = MainActivity.class.getSimpleName() + "_TAG";
+    private ZXingScannerView zXingScannerView;
+    private PermissionManager permissionManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        permissionManager = new PermissionManager(this);
+        permissionManager.checkPermission();
+    }
+
+    public void scan(View view) {
+
+        zXingScannerView = new ZXingScannerView(getApplicationContext());
+        setContentView(zXingScannerView);
+        zXingScannerView.setResultHandler(this);
+        zXingScannerView.startCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        zXingScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+
+        Toast.makeText(this, result.getText(), Toast.LENGTH_SHORT).show();
+        zXingScannerView.resumeCameraPreview(this);
+    }
+
+    @Override
+    public void onPermissionResult(boolean isGranted) {
+        Log.d(TAG, "onPermissionResult: ");
+    }
+}
